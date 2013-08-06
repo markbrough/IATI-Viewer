c2938f9bf11f691b333fe44468d9345c4a142a26
James Padolsey <github@padolsey.net>
2013-08-06 21:42:47 +0100

    more info in report view

--- js/ReportView.js
+++ js/ReportView.js
@@ -23,19 +23,34 @@ define(function(require) {
 		render: function() {
 			this.$el.html( template({
 				title: this.activity.title.content,
+				description: this.activity.description.content,
+				participatingOrgs: this.activity['participating-org'],
 				stats: [
 					{
 						name: 'Transactions total', 
 						value: formatCurrency( this.getTransactionTotal() ) + ' USD' 
+					},
+					{
+						name: 'Disbursement total', 
+						value: formatCurrency(this.getTransactionTotal(function(t) {
+							return t['transaction-type'].code === 'D'
+						})) + ' USD' 
+					},
+					{
+						name: 'Commitment total', 
+						value: formatCurrency(this.getTransactionTotal(function(t) {
+							return t['transaction-type'].code === 'C'
+						})) + ' USD' 
 					}
 				]
 			}) );
 		},
 
-		getTransactionTotal: function() {
+		getTransactionTotal: function(filter) {
 			var sum = 0;
 			_.each(this.activity.transaction, function(t) {
 				if (t.value.currency !== 'USD') return;
+				if (filter && !filter(t)) return;
 				sum += +t.value.content;
 			});
 			return sum;
